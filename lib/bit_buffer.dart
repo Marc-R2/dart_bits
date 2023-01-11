4c450f9581a4b63598809a8e47467a773fca39ba
cyberpwn <danielmillst@gmail.com>
2023-01-11 03:34:26 -0500

    wppppp

--- lib/bit_buffer.dart
+++ lib/bit_buffer.dart
@@ -2,8 +2,27 @@ import 'dart:math';
 import 'dart:typed_data';
 
 const int bitsPerInt = 64;
+const List<int> stepList1b = [32, 64];
+const List<int> stepList2b = [8, 16, 32, 64];
+const List<int> stepDecList1b = [2, 4];
+const List<int> stepDecList2b = [1, 2, 3, 4];
 
-int getBitsNeeded(int value) {
+int _bitStep(int value, List<int> bitSteps) {
+  int step = -1;
+  for (int i = 0; i < bitSteps.length; i++) {
+    if (_getBitsNeeded(value.abs()) <= bitSteps[i]) {
+      step = i;
+      break;
+    }
+  }
+
+  return step;
+}
+
+int _getMaxBitsNeededForDigits(int decimals) =>
+    _getBitsNeeded(int.parse("9" * decimals));
+
+int _getBitsNeeded(int value) {
   assert(value >= 0, "Value must be positive");
   int bits = 0;
   while (value > 0) {
@@ -13,11 +32,40 @@ int getBitsNeeded(int value) {
   return max(1, bits);
 }
 
+class DummyBitBufferWriter extends BitBufferWriter {
+  DummyBitBufferWriter() : super(BitBuffer());
+
+  @override
+  void writeBits(int value, int bits) => _i(bits);
+
+  @override
+  void writeBit(bool state) => _i();
+}
+
+typedef BitCodecWriter<T> = void Function(BitBufferWriter writer, T t);
+typedef BitCodecReader<T> = T Function(BitBufferReader reader);
+
+class BitCodec<T> {
+  final BitCodecWriter<T> writer;
+  final BitCodecReader<T> reader;
+
+  BitCodec({required this.writer, required this.reader});
+}
+
 class BitBufferWriter {
   int _written = 0;
   final BitBuffer _buffer;
 
   BitBufferWriter(this._buffer);
+
+  void seek(int position) {
+    _written = position;
+    if (_written > _buffer._size) {
+      _buffer.allocate(_written - _buffer._size);
+    }
+  }
+
+  int getBitsWritten() => _written;
 
   int _i([int f = 1]) {
     int t = _written;
@@ -25,35 +73,165 @@ class BitBufferWriter {
     return t;
   }
 
+  void allocateIfNeeded(int bits) {
+    if (_written + bits > _buffer._size) {
+      _buffer.allocate(bits);
+    }
+  }
+
   void writeBits(int value, int bits) {
-    _buffer.allocate(bits);
+    if (bits < _getBitsNeeded(value.abs())) {
+      throw Exception(
+          "Value $value is too large for $bits bits. You would need ${_getBitsNeeded(value.abs())} bits to write $value. (or write up to ${pow(2, bits) - 1}) bits");
+    }
+
+    allocateIfNeeded(bits);
     _buffer.setBits(_i(bits), bits, value);
   }
 
   void writeBit(bool state) {
-    _buffer.allocate(1);
+    allocateIfNeeded(1);
     _buffer.setBit(_i(), state);
   }
 
-  void writeDouble(double value,
-      {bool signed = true, int bits = 64, int decimalBits = 16}) {
+  void writeBestCodec<T>(List<BitCodec<T>> codecs, T value) {
+    int bestCodec = getBestCodec(codecs, value);
+    writeInt(bestCodec, signed: false, bits: _getBitsNeeded(codecs.length - 1));
+    writeCodec(codecs[bestCodec], value);
+  }
+
+  int getBestCodec<T>(List<BitCodec<T>> codecs, T value) {
+    int smallest = -1;
+    int bestCodec = -1;
+
+    for (int i = 0; i < codecs.length; i++) {
+      int size = getCodecWrittenSize(codecs[i], value);
+
+      if (size < 0) {
+        continue;
+      }
+
+      if (smallest == -1 || size < smallest) {
+        smallest = size;
+        bestCodec = i;
+      }
+    }
+
+    if (bestCodec == -1) {
+      throw Exception("No codec could write $value");
+    }
+
+    return bestCodec;
+  }
+
+  void writeCodec<T>(BitCodec<T> method, T value) => method.writer(this, value);
+
+  int getCodecWrittenSize<T>(BitCodec<T> method, T value) {
+    try {
+      return (DummyBitBufferWriter()..writeCodec(method, value))
+          .getBitsWritten();
+    } catch (e) {
+      return -1;
+    }
+  }
+
+  void writeLinearVarDouble(double value,
+      {bool signed = true, int maxBits = 64, int maxDecimal = 8}) {
     if (signed) {
       writeBit(value > 0);
     }
-    writeInt(value.abs().truncate(), signed: false, bits: bits);
-// dont write int just truncate thingy
-    double v = value.abs() - value.abs().truncate();
+    writeLinearVarInt(value.truncate().abs(), signed: false, maxBits: maxBits);
+    double decimal = double.parse(
+        (value.abs() - value.truncate().abs()).toStringAsFixed(maxDecimal));
+    int exponent = 0;
 
-    while (v != v.truncate() && getBitsNeeded(v.truncate()) <= decimalBits) {
-      v *= 10;
+    while (decimal != decimal.truncate() && exponent < maxDecimal) {
+      exponent++;
+      decimal *= 10;
     }
 
-    writeInt(v.truncate(), signed: false, bits: decimalBits);
+    writeLinearVarInt(exponent,
+        signed: false, maxBits: _getBitsNeeded(maxDecimal));
+    writeLinearVarInt(decimal.truncate(),
+        signed: false, maxBits: _getMaxBitsNeededForDigits(maxDecimal));
+  }
+
+  void writeSteppedVarDouble(double value,
+      {bool signed = true,
+      List<int> bitLimits = stepList2b,
+      List<int> decimalBitLimits = stepDecList2b}) {
+    if (signed) {
+      writeBit(value > 0);
+    }
+    int maxDecimal = pow(2, decimalBitLimits.last).toInt() - 1;
+    writeSteppedVarInt(value.truncate().abs(),
+        signed: false, bitLimits: bitLimits);
+    double decimal = double.parse(
+        (value.abs() - value.truncate().abs()).toStringAsFixed(maxDecimal));
+    int exponent = 0;
+
+    while (decimal != decimal.truncate() && exponent < maxDecimal) {
+      exponent++;
+      decimal *= 10;
+    }
+
+    writeSteppedVarInt(exponent, signed: false, bitLimits: decimalBitLimits);
+    writeInt(decimal.truncate(),
+        signed: false,
+        bits: _getMaxBitsNeededForDigits(
+            pow(2, decimalBitLimits[_bitStep(exponent, decimalBitLimits)])
+                    .toInt() -
+                1));
+  }
+
+  void writeDouble(double value,
+      {bool signed = true, int bits = 64, int maxDecimal = 8}) {
+    if (signed) {
+      writeBit(value > 0);
+    }
+    writeInt(value.truncate().abs(), signed: false, bits: bits);
+    double decimal = double.parse(
+        (value.abs() - value.truncate().abs()).toStringAsFixed(maxDecimal));
+    int exponent = 0;
+
+    while (decimal != decimal.truncate() && exponent < maxDecimal) {
+      exponent++;
+      decimal *= 10;
+    }
+
+    writeInt(exponent, signed: false, bits: _getBitsNeeded(maxDecimal));
+    writeInt(decimal.truncate(),
+        signed: false, bits: _getMaxBitsNeededForDigits(maxDecimal));
+  }
+
+  void writeSteppedVarInt(int value,
+      {bool signed = true, List<int> bitLimits = stepList2b}) {
+    if (signed) {
+      writeBit(value > 0);
+    }
+
+    int step = _bitStep(value, bitLimits);
+
+    if (step == -1) {
+      throw new Exception("Value too large");
+    }
+
+    writeInt(step, signed: false, bits: _getBitsNeeded(bitLimits.length - 1));
+    writeInt(value.abs(), signed: false, bits: bitLimits[step]);
+  }
+
+  void writeLinearVarInt(int value, {bool signed = true, int maxBits = 64}) {
+    if (signed) {
+      writeBit(value > 0);
+    }
+    int bits = _getBitsNeeded(value.abs());
+    writeInt(bits, signed: false, bits: _getBitsNeeded(maxBits));
+    writeInt(value.abs(), signed: false, bits: bits);
   }
 
   void writeInt(int value, {bool signed = true, int bits = 64}) {
     if (signed) {
-      writeBit(bits > 0);
+      writeBit(value > 0);
     }
     writeBits(value.abs(), bits);
   }
@@ -63,27 +241,100 @@ class BitBufferReader {
   int _read = 0;
   final BitBuffer _buffer;
 
+  void skip(int bits) => _read += bits;
+
+  void seekTo(int bit) => _read = bit;
+
   int _i([int f = 1]) {
     int t = _read;
     _read += f;
     return t;
   }
 
-  double readDouble({bool signed = true, int bits = 64, int decimalBits = 16}) {
+  T readBestCodec<T>(List<BitCodec<T>> codecs) => readCodec(
+      codecs[readInt(signed: false, bits: _getBitsNeeded(codecs.length - 1))]);
+
+  T readCodec<T>(BitCodec<T> method) => method.reader(this);
+
+  double readDouble({bool signed = true, int bits = 64, int maxDecimal = 8}) {
     double sign = signed
         ? !readBit()
             ? -1.0
             : 1.0
         : 1.0;
     double base = readInt(signed: false, bits: bits).toDouble();
-    int m = readInt(signed: false, bits: decimalBits);
-    base += m.toDouble() / pow(10, m.toString().length);
-    return base * sign;
+    int exponent = readInt(signed: false, bits: _getBitsNeeded(maxDecimal));
+    int decimal =
+        readInt(signed: false, bits: _getMaxBitsNeededForDigits(maxDecimal));
+    return double.parse((sign * (base + (decimal / pow(10, exponent))))
+        .toStringAsFixed(maxDecimal));
+  }
+
+  double readSteppedVarDouble(
+      {bool signed = true,
+      List<int> bitLimits = stepList2b,
+      List<int> decimalBitLimits = stepDecList2b}) {
+    double sign = signed
+        ? !readBit()
+            ? -1.0
+            : 1.0
+        : 1.0;
+
+    int maxDecimal = pow(2, decimalBitLimits.last).toInt() - 1;
+    double base =
+        readSteppedVarInt(signed: false, bitLimits: bitLimits).toDouble();
+    int exponent =
+        readSteppedVarInt(signed: false, bitLimits: decimalBitLimits);
+    int step = _bitStep(exponent, decimalBitLimits);
+    int decimal = readInt(
+        signed: false,
+        bits: _getMaxBitsNeededForDigits(
+            pow(2, decimalBitLimits[step]).toInt() - 1));
+    return double.parse((sign * (base + (decimal / pow(10, exponent))))
+        .toStringAsFixed(maxDecimal));
+  }
+
+  double readLinearVarDouble(
+      {bool signed = true, int maxBits = 64, int maxDecimal = 8}) {
+    double sign = signed
+        ? !readBit()
+            ? -1.0
+            : 1.0
+        : 1.0;
+    double base = readLinearVarInt(signed: false, maxBits: maxBits).toDouble();
+    int exponent =
+        readLinearVarInt(signed: false, maxBits: _getBitsNeeded(maxDecimal));
+    int decimal = readLinearVarInt(
+        signed: false, maxBits: _getMaxBitsNeededForDigits(maxDecimal));
+    return double.parse((sign * (base + (decimal / pow(10, exponent))))
+        .toStringAsFixed(maxDecimal));
   }
 
   bool readBit() => _buffer.getBit(_i());
 
   int readBits(int bits) => _buffer.getBits(_i(bits), bits);
+
+  int readSteppedVarInt(
+      {bool signed = true, List<int> bitLimits = stepList2b}) {
+    int sign = signed
+        ? !readBit()
+            ? -1
+            : 1
+        : 1;
+    int step =
+        readInt(signed: false, bits: _getBitsNeeded(bitLimits.length - 1));
+    return sign * readInt(signed: false, bits: bitLimits[step]);
+  }
+
+  int readLinearVarInt({bool signed = true, int maxBits = 64}) {
+    int sign = signed
+        ? !readBit()
+            ? -1
+            : 1
+        : 1;
+    int bits = readInt(signed: false, bits: _getBitsNeeded(maxBits));
+    return sign * readInt(signed: false, bits: bits);
+  }
 
   int readInt({bool signed = true, int bits = 64}) =>
       (signed && !readBit()) ? -readBits(bits) : readBits(bits);
@@ -98,6 +349,8 @@ class BitBuffer {
   BitBuffer();
 
   BitBufferWriter writer() => BitBufferWriter(this);
+
+  BitBufferWriter append() => writer()..seek(_size);
 
   BitBufferReader reader() => BitBufferReader(this);
 
@@ -122,7 +375,7 @@ class BitBuffer {
       {int bitsPerIndex = bitsPerInt, int? trueSize}) {
     BitBuffer bb = BitBuffer();
     bb.allocate(trueSize ?? data.length * bitsPerIndex);
-    for (int i = 0; i < bb._size; i++) {
+    for (int i = 0; i < bb._size - 1; i++) {
       int localBit = i & (bitsPerIndex - 1);
       int localIndex = i >> bitsPerIndex;
       bb.setBit(i, data[localIndex] & (1 << localBit) != 0);
--- test/bits_test_2.dart
+++ test/bits_test_2.dart
@@ -1,18 +1,112 @@
-import 'dart:convert';
 import 'dart:math';
 
 import 'package:bits/bit_buffer.dart';
 import 'package:bits/bits.dart';
-import 'package:crypto/crypto.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('Test', () {
-    BitBuffer bb = BitBuffer();
-    bb.writer().writeDouble(3.1111111111111);
-    bb.printBinary();
-    print(bb.reader().readDouble());
+  List<int> testInts = [
+    0,
+    1,
+    -1,
+    3854,
+    -3346,
+    1234567890,
+    -1234567890,
+    2147483647,
+    -2147483748,
+    4294967295,
+    -4294967295,
+    9223372036854775807,
+    -922337203685477580
+  ];
+  List<double> testDoubles = [
+    0,
+    1,
+    -1,
+    0.1,
+    3.5584,
+    2.345666,
+    2838455.12,
+    29345588585.1118,
+    -28388383838383.7771
+  ];
+
+  test('Basic Copying & Loading & Exporting', () {
+    int v = -23495995555553;
+    BitBuffer buf = BitBuffer();
+    buf.writer().writeInt(v);
+    buf.writer().writeInt(-v * 33);
+    buf.writer().writeInt(16);
+    BitBuffer buf2 = BitBuffer.fromBB(buf);
+    expect(buf2.getLongs(), equals(buf.getLongs()));
   });
+
+  for (double i in testDoubles) {
+    test(
+        'Test writeDouble',
+        () => expect(
+            i,
+            equals(
+                (BitBuffer()..writer().writeDouble(i)).reader().readDouble()),
+            reason: 'Failed to writeDouble ${i}'));
+    test(
+        'Test writeUDouble',
+        () => expect(
+            i.abs(),
+            equals((BitBuffer()..writer().writeDouble(i.abs(), signed: false))
+                .reader()
+                .readDouble(signed: false)),
+            reason: 'Failed to writeDouble ${i.abs()}'));
+    test(
+        'Test writeLinearVarDouble',
+        () => expect(
+            i,
+            equals((BitBuffer()..writer().writeLinearVarDouble(i))
+                .reader()
+                .readLinearVarDouble()),
+            reason: 'Failed to writeDouble ${i}'));
+    test(
+        'Test writeSteppedVarDouble',
+        () => expect(
+            i,
+            equals((BitBuffer()..writer().writeSteppedVarDouble(i))
+                .reader()
+                .readSteppedVarDouble()),
+            reason: 'Failed to writeDouble ${i}'));
+  }
+
+  for (int i in testInts) {
+    test(
+        'Test writeInt',
+        () => expect(
+            i, equals((BitBuffer()..writer().writeInt(i)).reader().readInt()),
+            reason: 'Failed to writeInt ${i}'));
+    test(
+        'Test writeUInt',
+        () => expect(
+            i.abs(),
+            equals((BitBuffer()..writer().writeInt(i.abs(), signed: false))
+                .reader()
+                .readInt(signed: false)),
+            reason: 'Failed to writeInt ${i.abs()}'));
+    test(
+        'Test writeLinearVarInt',
+        () => expect(
+            i,
+            equals((BitBuffer()..writer().writeLinearVarInt(i))
+                .reader()
+                .readLinearVarInt()),
+            reason: 'Failed to writeLinearVarInt ${i}'));
+    test(
+        'Test writeSteppedVarInt',
+        () => expect(
+            i,
+            equals((BitBuffer()..writer().writeSteppedVarInt(i))
+                .reader()
+                .readSteppedVarInt()),
+            reason: 'Failed to writeSteppedVarInt ${i}'));
+  }
 }
 
 Iterable<int> randomInts(int count) sync* {
